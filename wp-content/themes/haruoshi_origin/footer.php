566c2ae02465eb072b563264b1278b58770f9a63
oshimaharuna <ooooooshim115@gmail.com>
2022-02-23 20:39:54 +0900

    ヘッダーとフッター作成

---
+++ wp-content/themes/haruoshi_origin/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <p>&copy; haruoshi. 2022.</p>
+</footer>
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ wp-content/themes/haruoshi_origin/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>ORIGIN</title>
+    <meta name="description" content="haruoshiの練習サイト">
+    <p>haruoshiの練習サイト</p>
+    <?php wp_head(); ?>
+</head>
+
+<body>
