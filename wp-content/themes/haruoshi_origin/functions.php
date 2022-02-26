292e1409ffdc721297d82273b4c58e79648cdab9
oshimaharuna <ooooooshim115@gmail.com>
2022-02-26 19:09:51 +0900

    フォーマットかけた

--- wp-content/themes/haruoshi_origin/functions.php
+++ wp-content/themes/haruoshi_origin/functions.php
@@ -1,33 +1,41 @@
 <?php
-function enqueue_style()
-{
-    wp_enqueue_style(
-        'common', 
-        get_stylesheet_directory_uri() . '/dist/css/common.css', 
-        array(),
-        filemtime(get_stylesheet_directory() . '/dist/css/common.css'),
-    );
-    wp_enqueue_style(
-        'header', 
-        get_stylesheet_directory_uri() . '/dist/css/header.css', 
-        array(),
-        filemtime(get_stylesheet_directory() . '/dist/css/header.css'),
-    );
-    wp_enqueue_style(
-        'footer', 
-        get_stylesheet_directory_uri() . '/dist/css/footer.css', 
-        array(),
-        filemtime(get_stylesheet_directory() . '/dist/css/footer.css'),
-    );
+/**
+ * This is function.php
+ *
+ * @package haruoshi-origin
+ */
 
-    if(is_home()):
-        wp_enqueue_style(
-            'home', 
-            get_stylesheet_directory_uri() . '/dist/css/home.css', 
-            array(),
-            filemtime(get_stylesheet_directory() . '/dist/css/home.css'),
-        );
-    endif;
+/**
+ * Styleの読み込み
+ */
+function enqueue_style() {
+	wp_enqueue_style(
+		'common',
+		get_stylesheet_directory_uri() . '/dist/css/common.css',
+		array(),
+		filemtime( get_stylesheet_directory() . '/dist/css/common.css' ),
+	);
+	wp_enqueue_style(
+		'header',
+		get_stylesheet_directory_uri() . '/dist/css/header.css',
+		array(),
+		filemtime( get_stylesheet_directory() . '/dist/css/header.css' ),
+	);
+	wp_enqueue_style(
+		'footer',
+		get_stylesheet_directory_uri() . '/dist/css/footer.css',
+		array(),
+		filemtime( get_stylesheet_directory() . '/dist/css/footer.css' ),
+	);
+
+	if ( is_home() ) :
+		wp_enqueue_style(
+			'home',
+			get_stylesheet_directory_uri() . '/dist/css/home.css',
+			array(),
+			filemtime( get_stylesheet_directory() . '/dist/css/home.css' ),
+		);
+	endif;
 }
 
-add_action('wp_enqueue_scripts', 'enqueue_style');
+add_action( 'wp_enqueue_scripts', 'enqueue_style' );
--- wp-content/themes/haruoshi_origin/header.php
+++ wp-content/themes/haruoshi_origin/header.php
@@ -1,15 +1,24 @@
+<?php
+/**
+ * This is header.php
+ *
+ * @package haruoshi-origin
+ */
+
+?>
+
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>ORIGIN</title>
-    <meta name="description" content="haruoshiの練習サイト">
-    <?php wp_head(); ?>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>ORIGIN</title>
+	<meta name="description" content="haruoshiの練習サイト">
+	<?php wp_head(); ?>
 </head>
 
 <body>
 <header class="site-header">
-    header
+	header
 </header>
 <main class="main" role="main">
