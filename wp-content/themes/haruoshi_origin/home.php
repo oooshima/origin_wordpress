63790df45f585e7f59f0c2815389bc3b2856c1c1
oshimaharuna <ooooooshim115@gmail.com>
2022-03-03 01:00:07 +0900

    topページに表示した

--- wp-content/themes/haruoshi_origin/home.php
+++ wp-content/themes/haruoshi_origin/home.php
@@ -8,8 +8,17 @@
 get_header(); ?>
 
 <article class="container">
-	<section>
-		hoge
+	<section class="news content">
+		<h1 class="content__title">News</h1>
+		<?php
+			$posts_array = get_posts( array( 'post_type' => 'news' ) );
+		?>
+		<?php foreach ( $posts_array as $post ) : setup_postdata( $post ); ?>
+			<div class="news__article">
+				<p><?php the_time( 'Y.m.d' )?></p>
+				<a class="news__article__title" href="<?php the_permalink() ?>"><?php the_title(); ?></a><br/>
+			</div>
+		<?php endforeach; wp_reset_postdata(); ?>
 	</section>
 </article>
 
