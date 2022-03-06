a80bf68a61ba3afdf713b086b5c900d478d39547
oshimaharuna <ooooooshim115@gmail.com>
2022-03-06 16:40:03 +0900

    投稿がない時は表示しない

--- wp-content/themes/haruoshi_origin/home.php
+++ wp-content/themes/haruoshi_origin/home.php
@@ -12,13 +12,14 @@ get_header(); ?>
 		<h1 class="content__title">News</h1>
 		<?php
 			$posts_array = get_posts( array( 'post_type' => 'news' ) );
+			if (!empty($posts_array)):
 		?>
 		<?php foreach ( $posts_array as $post ) : setup_postdata( $post ); ?>
 			<div class="news__article">
 				<p><?php the_time( 'Y.m.d' )?></p>
 				<a class="news__article__title" href="<?php the_permalink() ?>"><?php the_title(); ?></a><br/>
 			</div>
-		<?php endforeach; wp_reset_postdata(); ?>
+		<?php endforeach; endif; wp_reset_postdata(); ?>
 	</section>
 </article>
 
