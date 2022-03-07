f8d2d026405f72a84a9f82f1a9a6a1de918674f9
oshimaharuna <ooooooshim115@gmail.com>
2022-03-07 13:33:25 +0900

    WP_Queryを使う

--- wp-content/themes/haruoshi_origin/home.php
+++ wp-content/themes/haruoshi_origin/home.php
@@ -11,16 +11,16 @@ get_header(); ?>
 	<section class="news">
 		<h1 class="content-title">News</h1>
 		<?php
-			$posts_array = get_posts( array( 'post_type' => 'news' ) );
-			if (!empty($posts_array)):
+			$news_query = new WP_Query( array( 'post_type' => 'news' ) );
+			if ( $news_query->have_posts() ):
 		?>
 		<ul class="news__article-list">
-		<?php foreach ( $posts_array as $post ) : setup_postdata( $post ); ?>
+		<?php while ( $news_query->have_posts() ) : $news_query->the_post(); ?>
 			<li class="news__article">
 				<time><?php the_time( 'Y.m.d' )?></time>
 				<p class="news__article-title" href="<?php the_permalink() ?>"><?php the_title(); ?></p>
 			</li>
-		<?php endforeach; endif; wp_reset_postdata(); ?>
+		<?php endwhile; endif; wp_reset_postdata(); ?>
 		</ul>
 	</section>
 </article>
