232644312d3fe082ce35db69a95859e5f4520290
oshimaharuna <ooooooshim115@gmail.com>
2022-03-07 20:49:00 +0900

    フォーマット

--- wp-content/themes/haruoshi_origin/home.php
+++ wp-content/themes/haruoshi_origin/home.php
@@ -11,25 +11,27 @@ get_header(); ?>
 	<section class="news section-wrapper">
 		<h1 class="section-title">News</h1>
 		<?php
-			$news_query = new WP_Query( 
-				array( 
-					'post_type' => 'news' ,
-					'posts_per_page' => 3,
-					'order' => 'ASC',
-					'orderby' => 'date'
-					) 
-			);
-			if ( $news_query->have_posts() ):
+		$news_query = new WP_Query( 
+			array( 
+				'post_type' => 'news' ,
+				'posts_per_page' => 3,
+				'order' => 'ASC',
+				'orderby' => 'date'
+				) 
+		);
+		if ( $news_query->have_posts() ):
 		?>
-		<ul class="news__article-list">
-		<?php while ( $news_query->have_posts() ) : $news_query->the_post(); ?>
-			<li class="news__article">
-				<time><?php the_time( 'Y.m.d' )?></time>
-				<p class="news__article-title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></p>
-			</li>
-		<?php endwhile; endif; wp_reset_postdata(); ?>
-		</ul>
-		<p class="seemore-link"><a href="<?php echo esc_url(home_url( '/' )); ?>news">See More</a></p>
+			<ul class="news__article-list">
+			<?php while ( $news_query->have_posts() ) : $news_query->the_post(); ?>
+				<li class="news__article">
+					<time><?php the_time( 'Y.m.d' )?></time>
+					<p class="news__article-title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></p>
+				</li>
+			<?php endwhile; ?>
+			</ul>
+		<?php endif; 
+		wp_reset_postdata(); ?>
+		<p class="seemore-link"><a href="<?= get_post_type_archive_link( 'news' ); ?>">See More</a></p>
 	</section>
 </article>
 
