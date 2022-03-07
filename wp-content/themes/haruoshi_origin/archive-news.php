071a411942d866fea4fe6fb35ce164c1cf734cd7
oshimaharuna <ooooooshim115@gmail.com>
2022-03-07 20:34:29 +0900

    メインループにした

--- wp-content/themes/haruoshi_origin/archive-news.php
+++ wp-content/themes/haruoshi_origin/archive-news.php
@@ -10,18 +10,19 @@ get_header(); ?>
 <article class="container">
 	<section class="news">
 		<h1 class="content-title">News</h1>
-		<?php
-			$news_query = new WP_Query( array( 'post_type' => 'news' ) );
-			if ( $news_query->have_posts() ):
+		<?php if ( have_posts() ):?>
+			<ul class="news__article-list">
+			<?php while ( have_posts() ) : the_post(); ?>
+				<li class="news__article">
+					<time><?php the_time( 'Y.m.d' )?></time>
+					<p class="news__article-title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></p>
+				</li>
+			<?php 
+			endwhile; ?>
+			</ul>
+		<?php endif; 
+		wp_reset_postdata(); 
 		?>
-		<ul class="news__article-list">
-		<?php while ( $news_query->have_posts() ) : $news_query->the_post(); ?>
-			<li class="news__article">
-				<time><?php the_time( 'Y.m.d' )?></time>
-				<p class="news__article-title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></p>
-			</li>
-		<?php endwhile; endif; wp_reset_postdata(); ?>
-		</ul>
 	</section>
 </article>
 
