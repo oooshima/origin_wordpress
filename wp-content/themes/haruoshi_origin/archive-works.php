3fa6155e234755cfc5f6a20032049983f0cc9c24
oshimaharuna <ooooooshim115@gmail.com>
2022-03-20 19:22:43 +0900

    aタグ追加

--- wp-content/themes/haruoshi_origin/archive-works.php
+++ wp-content/themes/haruoshi_origin/archive-works.php
@@ -13,13 +13,15 @@ get_header(); ?>
 		<?php if ( have_posts() ):?>
 			<ul class="works__article-list">
 				<?php while ( have_posts() ) : the_post(); ?>
-					<a class="works__article" href="<?php the_permalink() ?>">
-						<?php if ( has_post_thumbnail() ): ?>
-							<img class="works__article-thumbnail" src="<?= the_post_thumbnail_url(); ?>" alt="thumbnail">
-						<?php else: ?>
-							<img class="works__article-thumbnail" src="<?= esc_url( get_stylesheet_directory_uri() . '/img/dummy.jpg' ); ?>" alt="dummy-thumbnail">
-						<?php endif; ?>
-					</a>
+					<li class="works__article">
+						<a href="<?php the_permalink() ?>">
+							<?php if ( has_post_thumbnail() ): ?>
+								<img class="works__article-thumbnail" src="<?= the_post_thumbnail_url(); ?>" alt="thumbnail">
+							<?php else: ?>
+								<img class="works__article-thumbnail" src="<?= esc_url( get_stylesheet_directory_uri() . '/img/dummy.jpg' ); ?>" alt="dummy-thumbnail">
+							<?php endif; ?>
+						</a>
+					</li>
 				<?php endwhile; ?>
 			</ul>
 		<?php endif;
