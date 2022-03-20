729d01774cb4a2581699a13e47c97b63ca58f6f8
oshimaharuna <ooooooshim115@gmail.com>
2022-03-20 19:32:58 +0900

    日付をtimeタグにする

--- wp-content/themes/haruoshi_origin/single-works.php
+++ wp-content/themes/haruoshi_origin/single-works.php
@@ -9,7 +9,7 @@ get_header(); ?>
 
 <article class="container">
 	<h1 class="container__title"><?php the_title(); ?></h1>
-	<p class="container__date"><?php the_time( 'Y/m/d' ); ?></p>
+	<time class="container__date"><?php the_time( 'Y/m/d' ); ?></time>
 	<?php if ( has_post_thumbnail() ): ?>
 		<img class="container__thumbnail" src="<?= the_post_thumbnail_url(); ?>" alt="thumbnail">
 	<?php endif; ?>
