526d11003fa39e89aa276b8f797b4ee7c71e67e7
oshimaharuna <ooooooshim115@gmail.com>
2022-03-27 01:21:05 +0900

    ブレークポイント打てるように設定追加

--- wp-content/themes/haruoshi_origin/header.php
+++ wp-content/themes/haruoshi_origin/header.php
@@ -15,6 +15,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>ORIGIN</title>
     <meta name="description" content="haruoshiの練習サイト">
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
     <?php wp_head(); ?>
 </head>
 
