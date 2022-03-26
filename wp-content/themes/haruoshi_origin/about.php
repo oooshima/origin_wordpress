53f5730c15ecf277b79a71e5633fe831cc6bbfe3
oshimaharuna <ooooooshim115@gmail.com>
2022-03-27 01:24:06 +0900

    ヘッダーサイズ変更に伴う修正

--- wp-content/themes/haruoshi_origin/about.php
+++ wp-content/themes/haruoshi_origin/about.php
@@ -9,7 +9,7 @@
 
 get_header(); ?>
 
-<article class="container">
+<article class="container section-wrapper-sp">
     <h1 class="section-title">About</h1>
     <div class="container__contents"><?php the_content(); ?></div>
 </article>
