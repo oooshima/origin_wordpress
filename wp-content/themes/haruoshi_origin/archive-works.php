279ec1f86ece34126a68ea0d157d1eb3747c47e0
oshimaharuna <ooooooshim115@gmail.com>
2022-04-11 19:05:35 +0900

    メインループに戻す

--- wp-content/themes/haruoshi_origin/archive-works.php
+++ wp-content/themes/haruoshi_origin/archive-works.php
@@ -10,20 +10,9 @@ get_header(); ?>
 <article class="container">
     <section class="works section-wrapper">
         <h1 class="section-title">Works</h1>
-        <?php
-        $works_query = new WP_Query( 
-            array( 
-                'post_type' => 'works',
-                'posts_per_page' => 6,
-                'order' => 'ASC',
-                'orderby' => 'date',
-                'paged' => get_query_var('paged')
-            )
-        );
-        if ( $works_query->have_posts() ):
-        ?>
+        <?php if ( have_posts() ):?>
         <ul class="works__article-list">
-            <?php while ( $works_query->have_posts() ) : $works_query->the_post(); ?>
+            <?php while ( have_posts() ) : the_post(); ?>
             <li class="works__article">
                 <a href="<?php the_permalink() ?>">
                     <?php if ( has_post_thumbnail() ): ?>
